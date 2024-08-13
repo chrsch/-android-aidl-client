fd6b66fedf4a2e4c0cf35f33a5782ad63de678d5
Christoph Schaefer <christophschaefer@posteo.de>
2024-08-13 23:05:38 +0200

    First impl of Weather feature

--- app/src/main/java/com/example/aidlclient/MainActivity.java
+++ app/src/main/java/com/example/aidlclient/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,18 +18,35 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.aidlserver.IAIDLColorInterface;
+import com.example.aidlserver.IWeather;
+import com.example.aidlserver.IWeatherCallback;
+import com.example.aidlserver.WeatherResult;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
     IAIDLColorInterface IAIDLColorService;
+    IWeather IWeatherService;
 
     private ServiceConnection mConnection = new ServiceConnection() {
         @Override
         public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
             IAIDLColorService = IAIDLColorInterface.Stub.asInterface(iBinder);
             Log.d(TAG, "Remote Color Service connected.");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
+
+    private ServiceConnection mWeatherConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            IWeatherService = IWeather.Stub.asInterface(iBinder);
+            Log.d(TAG, "Remote Weather Service connected.");
         }
 
         @Override
@@ -52,7 +70,13 @@ public class MainActivity extends AppCompatActivity {
         intent.setPackage("com.example.aidlserver");
 
         bindService(intent, mConnection, BIND_AUTO_CREATE);
-        Log.d(TAG, "bindservice called");
+        Log.d(TAG, "bindservice Color called");
+
+        Intent weatherIntent = new Intent("IWeatherService");
+        weatherIntent.setPackage("com.example.aidlserver");
+
+        bindService(weatherIntent, mWeatherConnection, BIND_AUTO_CREATE);
+        Log.d(TAG, "bindservice Weather called");
 
         Button b = findViewById((R.id.button));
 
@@ -65,8 +89,36 @@ public class MainActivity extends AppCompatActivity {
                         Log.e(TAG, "IAIDLColorService is null!");
                         return;
                     }
+                    if( IWeatherService == null ){
+                        Log.e(TAG, "IWeatherService is null!");
+                        return;
+                    }
                     int color = IAIDLColorService.getColor();
                     b.setBackgroundColor(color);
+
+                    IWeatherCallback callback = new IWeatherCallback() {
+                        @Override
+                        public void onSuccess(String response) throws RemoteException {
+                            Log.e(TAG, "IWeatherCallback success\n\n" + response);
+                        }
+
+                        @Override
+                        public void onException(String exception) throws RemoteException {
+                            Log.e(TAG, "IWeatherCallback exception\n\n" + exception);
+                        }
+
+                        @Override
+                        public IBinder asBinder() {
+                            return null;
+                        }
+                    };
+                    WeatherResult weatherResult = IWeatherService.getWeather(callback);
+                    String formattedDate = DateUtils.formatDateTime(
+                            getApplicationContext(),
+                            weatherResult.datetime,
+                            DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_TIME | DateUtils.FORMAT_SHOW_YEAR
+                    );
+                    Log.e(TAG, "IWeatherService call result " + formattedDate + " " + weatherResult.isSuccess);
                 } catch (RemoteException e) {
                     Log.e(TAG, e.toString());
                 }
